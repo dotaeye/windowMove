d420c4c82487782d037822d2ece7472e08a52915
dotaeye <dotaeye@yeah.net>
2018-12-12 21:51:25 +0800

    initial project

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,12 +28,37 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+ 
+      this.button2 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(177, 157);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Controls.Add(this.button2);
+ 
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button button2;
   }
 }
 
---
+++ WindowsFormsApp2/WinApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp2
+{
+  public class WinApi
+  {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+      public int Left;
+      public int Top;
+      public int Right;
+      public int Bottom;
+    }
+  }
+
+}
